8f24c64754194f6f005a580a97163a19695b7f8d
Randy Stauner <randy@magnificent-tears.com>
2015-07-17 21:37:59 -0700

    Limit friendly error message to the file we stat

--- test/rjs_test.js
+++ test/rjs_test.js
@@ -4,6 +4,10 @@ var Nut    = require('../lib'),
     path   = require('path'),
     expect = require('chai').expect;
 
+function testFile(basename) {
+  return path.resolve(__dirname, '..', 'tmp', 'data', basename);
+}
+
 describe('requirejs', function () {
 
   it('should not attempt r.js when no main.js is present', function(done) {
@@ -24,6 +28,27 @@ describe('requirejs', function () {
       expect( err.message ).to.equal('Cannot optimize; not found: "' + path.join(out, 'main.js') + '"');
       done();
     }).catch(done); // End suite on error.
+  });
+
+  it('should propagate the original error for any other case', function(done) {
+    var out = testFile('badout');
+
+    // We stat the file and provide a nicer message if it doesn't exist.
+    // Don't change the message if some other file doesn't exist.
+    // In this case the main.js exists
+    // but the index.html to be munged afterwards does not.
+
+    (new Nut({dir: out})).rjs()
+    .then(function () {
+      // Failure.
+      done(new Error("Expected error, got none."));
+    }, function(err) {
+      // Success?
+      expect( err.message ).to.not.match(/Cannot optimize;/);
+      expect( err.code ).to.equal('ENOENT');
+      done();
+    })
+    .catch(done);
   });
 
 });
