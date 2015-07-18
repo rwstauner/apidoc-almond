2efb9b6a995e417536e14207decd3fa0798879a9
Randy Stauner <randy@magnificent-tears.com>
2015-07-18 10:41:11 -0700

    Test that _command rethrows other errors

--- test/command_test.js
+++ test/command_test.js
@@ -4,12 +4,28 @@ var Nut    = require('../lib'),
 
 describe('command', function () {
 
+  var not_found_re = /^Command \S+ not found/;
+
   it('should mention module and command if not found', function (done) {
     new Nut()._command('apidoc-almond/a/bad/value')
     .catch(function (err) {
+      // General (use the same re for negation test later).
+      expect( err.message ).to.match(not_found_re);
+      // Specific.
       expect( err.message ).to.equal('Command "value" not found.  Is "apidoc-almond" installed?');
       done();
     }).catch(done);
+  });
+
+  it('should rethrow other errors', function (done) {
+    // Something that require() will barf on.
+    var badreqval = null;
+
+    new Nut()._command(badreqval)
+      .catch(function (err) {
+        expect( err.message ).to.not.match(not_found_re);
+        done();
+      }).catch(done);
   });
 
 });
